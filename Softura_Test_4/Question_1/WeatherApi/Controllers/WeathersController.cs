d51d757c717d30bc1ac2eb4e8427e778fc711372
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-05-21 18:51:40 +0530

    Add files via upload

---
+++ Softura_Test_4/Question_1/WeatherApi/Controllers/WeathersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WeatherApi.Models;
+
+namespace WeatherApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class WeathersController : ControllerBase
+    {
+       
+        private readonly WeatherContext _context;
+
+        public WeathersController(WeatherContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Weathers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Weather>>> GetWeathers()
+        {
+            return await _context.Weathers.ToListAsync();
+        }
+
+        // GET: api/Weathers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Weather>> GetWeather(int id)
+        {
+            var weather = await _context.Weathers.FindAsync(id);
+
+            if (weather == null)
+            {
+                return NotFound();
+            }
+
+            return weather;
+        }
+
+        // PUT: api/Weathers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWeather(int id, Weather weather)
+        {
+            if (id != weather.wid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(weather).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WeatherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+       
+
+        // DELETE: api/Weathers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWeather(int id)
+        {
+            var weather = await _context.Weathers.FindAsync(id);
+            if (weather == null)
+            {
+                return NotFound();
+            }
+
+            _context.Weathers.Remove(weather);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WeatherExists(int id)
+        {
+            return _context.Weathers.Any(e => e.wid == id);
+        }
+        [HttpGet]
+        [Route("{city}")]
+        public async Task<ActionResult<Weather>> GetWeatherByCity(string city)
+        {
+            var weather = await _context.Weathers.FindAsync(city);
+
+            if (weather == null)
+            {
+                return NotFound();
+            }
+
+            return weather;
+        }
+        private bool CityWeatherExists(string city)
+        {
+            return _context.Weathers.Any(e => e.City == city);
+        }
+        [HttpPut("{City}")]
+        public async Task<IActionResult> PutWeatherByCity(string city, Weather weather)
+        {
+            if (city!= weather.City)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(weather).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityWeatherExists(city))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Weather>> PostWeather(Weather weather)
+        {
+            _context.Weathers.Add(weather);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWeather", new { id = weather.wid }, weather);
+        }
+    }
+}
