b87fe03996c1affd0678d3a37ba744746edcfc8d
KRISHNA TRIPATHI <56901924+tripathik@users.noreply.github.com>
2021-05-21 18:53:17 +0530

    Add files via upload

---
+++ Softura_Test_4/Question_2/WeatherApi/Models/WeatherContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApi.Models
+{
+    public class WeatherContext:DbContext
+    {
+        public WeatherContext():base()
+        {
+
+        }
+        public WeatherContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Weather> Weathers { get; set; }
+    }
+}
